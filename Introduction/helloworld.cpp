ed59aa71aa8ab0893fec2ff9ef2ff4b0cb4608a8
Joel Howard <joelhowa@umflint.edu>
2020-01-06 23:36:52 -0500

    Hello world! example

---
+++ Introduction/helloworld.cpp
@@ -0,0 +1,26 @@
+/*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'/
+*
+*   ######      ######
+*   #######    #######
+*   ########  ########
+*    ################           Name:       Professor Joel Howard
+*    #### ###### ####           Class:      CSC-175 - Winter 20
+*    ####  ####  ####           Project:    Hello World! enhanced
+*   ######  ##  ######          
+*   ######      ######
+*   ######      ######
+*
+/'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*/
+
+// sets up our libraries which handle console input and output for the program
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // output hello and our user's name
+    cout << "Hello world!" << endl;
+
+    // exit our main function
+    return(0);
+}
