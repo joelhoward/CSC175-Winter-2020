6ed61ea8c294fc3478068d06250ccfad7aa9104f
Joel Howard <joelhowa@umflint.edu>
2020-01-30 10:15:26 -0500

    output and snippets for 1-30-20 lab
    utilizing fstream, switch, and do while loop

---
+++ 1-30-20/generated-output.cpp
@@ -0,0 +1,21 @@
+/*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'/
+*
+*	Name:		Joel Howard
+*	Class:		CSC-175
+*	Project: Assignment #4
+*	Due Date:	1-31-20
+*
+/'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*'*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+int main(void)
+{
+
+
+
+	return 0;
+}
