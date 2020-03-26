1f7e749f2b5656ffc0b1bf4892db0251718f80fa
Joel Howard <joelhowa@umflint.edu>
2020-03-26 11:50:44 -0400

    Pig Latin Lab skeleton start

---
+++ 3-26-20/PigLatinLabSkeleton.cpp
@@ -0,0 +1,38 @@
+/*
+The rules for pig latin are as follows:
+a.	For words that begin with consonants, move the leading consonant to
+ the end of the word and add "ay." Thus, "ball" becomes "allbay"; "button"
+becomes "uttonbay"; and so forth.
+b.	For words that begin with vowels, add "way" to the end.
+Thus, “all” becomes “allway”;  “one” becomes “oneway”; and so forth.
+*/
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/*
+    Purpose:            Convert a string to pig latin and output to screen
+    Pre-condition:      We need to include the string library in our app,
+                        and strInput needs to contain data.
+    Post-condition:     We will now have a string converted to pig latin.
+    Input:              (string) strInput - contains strings to be converted
+                            to pig latin.
+    Output:             To console, string that was converted to pig latin.
+*/
+void outputPigLatin(string strInput);
+
+int main() {
+
+    return 0;
+}
+
+
+
+
+void outputPigLatin(string strInput)
+{
+
+
+
+}
